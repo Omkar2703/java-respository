448d4adb40e9d1b893e0dbd95cf853a2cc3a1a39
Omkar2703 <omkar.ptl556@gmail.com>
2022-07-28 20:38:36 +0530

    Addition input user

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Add{
+public static void main(String [] args){
+Scanner sc = new scanner(System.in);
+System.out.println("Enter the first number:");
+int a = sc.nextInt();
+System.out.println("Enter the second number:");
+int b  = sc.nextInt();
+int c = a + b;
+System.out.println("Result:"+c);
+}
+}
