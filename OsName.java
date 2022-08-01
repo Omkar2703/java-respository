adb7f8a89c7f3b38d4e43226d8d1deaa399f6c53
Omkar2703 <omkar.ptl556@gmail.com>
2022-08-01 23:25:45 +0530

    OsName
    .

---
+++ OsName.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String operatingSystem = System.getProperty("os.name");
+    String operatingversion = System.getProperty("os.version");
+    System.out.println(operatingSystem);
+    System.out.println(operatingversion);
+
+  }
+}
