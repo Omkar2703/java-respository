4fb9f5194f11a3cf518b603c9e472b5839c0bb96
Omkar2703 <omkar.ptl556@gmail.com>
2022-08-01 23:03:16 +0530

    Number Pattern

---
+++ Numberpattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+public static void main(String [] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of rows:");
+int r = sc.nextInt();
+int i , j;
+for(i=1;i<=r;i++){
+for(j=1;j<=i;j++){
+System.out.print(j + " ");
+}
+System.out.println();
+}
+}
+}
