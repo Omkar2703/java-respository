59f3ee3f62c6decf42a10bf681fcecf6e636d08b
Omkar2703 <omkar.ptl556@gmail.com>
2022-08-12 00:03:18 +0530

    Finalpopulation
    codechef contest

---
+++ Finalpopulation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Finalpopulation{
+    public static void main(String args[]){
+        Scanner t = new Scanner (System.in);
+        int x = t.nextInt();
+        int y = t.nextInt();
+        int z = t.nextInt();
+        System.out.println((x-y+z)+" is the final population.");
+    }
+}
